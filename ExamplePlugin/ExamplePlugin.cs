abe9bedf5b4a53eee8fd529ce9f9979eeecac61f
Sean Hurt <smhurt@ksu.edu>
2024-06-22 20:05:27 -0500

    Removed most comments

--- ExamplePlugin/ExamplePlugin.cs
+++ ExamplePlugin/ExamplePlugin.cs
@@ -8,6 +8,7 @@ using UnityEngine.AddressableAssets;
 
 namespace ExamplePlugin
 {
+
     // This is an example plugin that can be put in
     // BepInEx/plugins/ExamplePlugin/ExamplePlugin.dll to test out.
     // It's a small plugin that adds a relatively simple item to the game,
@@ -45,14 +46,15 @@ namespace ExamplePlugin
 
         // We need our item definition to persist through our functions, and therefore make it a class field.
         private static ItemDef myItemDef;
+        private static BuffDef myBuffDef;
 
         // The Awake() method is run at the very start when the game is initialized.
         public void Awake()
         {
-            // Init our logging class so that we can properly log for debugging
+
+
             Log.Init(Logger);
 
-            // First let's define our item
             myItemDef = ScriptableObject.CreateInstance<ItemDef>();
 
             // Language Tokens, explained there https://risk-of-thunder.github.io/R2Wiki/Mod-Creation/Assets/Localization/
@@ -62,41 +64,28 @@ namespace ExamplePlugin
             myItemDef.descriptionToken = "EXAMPLE_CLOAKONKILL_DESC";
             myItemDef.loreToken = "EXAMPLE_CLOAKONKILL_LORE";
 
-            // The tier determines what rarity the item is:
+            
             // Tier1=white, Tier2=green, Tier3=red, Lunar=Lunar, Boss=yellow,
             // and finally NoTier is generally used for helper items, like the tonic affliction
-#pragma warning disable Publicizer001 // Accessing a member that was not originally public. Here we ignore this warning because with how this example is setup we are forced to do this
-            myItemDef._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/Base/Common/Tier2Def.asset").WaitForCompletion();
-#pragma warning restore Publicizer001
-            // Instead of loading the itemtierdef directly, you can also do this like below as a workaround
-            // myItemDef.deprecatedTier = ItemTier.Tier2;
+            #pragma warning disable Publicizer001
+            myItemDef._itemTierDef = Addressables.LoadAssetAsync<ItemTierDef>("RoR2/Base/Common/LunarDef.asset").WaitForCompletion();
+            #pragma warning restore Publicizer001
+            
 
-            // You can create your own icons and prefabs through assetbundles, but to keep this boilerplate brief, we'll be using question marks.
             myItemDef.pickupIconSprite = Addressables.LoadAssetAsync<Sprite>("RoR2/Base/Common/MiscIcons/texMysteryIcon.png").WaitForCompletion();
             myItemDef.pickupModelPrefab = Addressables.LoadAssetAsync<GameObject>("RoR2/Base/Mystery/PickupMystery.prefab").WaitForCompletion();
 
-            // Can remove determines
-            // if a shrine of order,
-            // or a printer can take this item,
-            // generally true, except for NoTier items.
+            // Can remove determines if a shrine of order, or a printer can take this item, generally true, except for NoTier items.
             myItemDef.canRemove = true;
 
-            // Hidden means that there will be no pickup notification,
-            // and it won't appear in the inventory at the top of the screen.
-            // This is useful for certain noTier helper items, such as the DrizzlePlayerHelper.
+            // Hidden means that there will be no pickup notification, and it won't appear in the inventory at the top of the screen.
             myItemDef.hidden = false;
 
-            // You can add your own display rules here,
-            // where the first argument passed are the default display rules:
-            // the ones used when no specific display rules for a character are found.
-            // For this example, we are omitting them,
-            // as they are quite a pain to set up without tools like https://thunderstore.io/package/KingEnderBrine/ItemDisplayPlacementHelper/
+            // https://thunderstore.io/package/KingEnderBrine/ItemDisplayPlacementHelper/
             var displayRules = new ItemDisplayRuleDict(null);
 
-            // Then finally add it to R2API
             ItemAPI.Add(new CustomItem(myItemDef, displayRules));
 
-            // But now we have defined an item, but it doesn't do anything yet. So we'll need to define that ourselves.
             GlobalEventManager.OnCharacterDeath += (GlobalEventManager.orig_OnCharacterDeath orig, RoR2.GlobalEventManager self, DamageReport report) =>
             {
                 GlobalEventManager_onCharacterDeath(report);
@@ -114,32 +103,22 @@ namespace ExamplePlugin
 
             var attackerCharacterBody = report.attackerBody;
 
-            // We need an inventory to do check for our item
             if (attackerCharacterBody.inventory)
             {
-                // Store the amount of our item we have
                 var garbCount = attackerCharacterBody.inventory.GetItemCount(myItemDef.itemIndex);
                 if (garbCount > 0 &&
-                    // Roll for our 50% chance.
                     Util.CheckRoll(50, attackerCharacterBody.master))
                 {
-                    // Since we passed all checks, we now give our attacker the cloaked buff.
-                    // Note how we are scaling the buff duration depending on the number of the custom item in our inventory.
                     attackerCharacterBody.AddTimedBuff(RoR2Content.Buffs.Cloak, 3 + garbCount);
                 }
             }
         }
 
-        // The Update() method is run on every frame of the game.
         private void Update()
         {
-            // This if statement checks if the player has currently pressed F2.
             if (Input.GetKeyDown(KeyCode.F2))
             {
-                // Get the player body to use a position:
                 var transform = PlayerCharacterMasterController.instances[0].master.GetBodyObject().transform;
-
-                // And then drop our defined item in front of the player.
 
                 Log.Info($"Player pressed F2. Spawning our custom item at coordinates {transform.position}");
                 PickupDropletController.CreatePickupDroplet(PickupCatalog.FindPickupIndex(myItemDef.itemIndex), transform.position, transform.forward * 20f);
